d9379c882472ca5a2a837cb4674b747967e20de4
Marina <marina.brener@gmail.com>
2021-06-11 15:47:28 -0400

    bakery app demo

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../assets/FluffyPhoto.jpg';
+import fluffyImage from '../../assets/FluffyPhoto.jpg';
 import classes from './Header.module.css';
 
 const Header = (props) => {
@@ -12,7 +12,7 @@ const Header = (props) => {
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
-        <img src={mealsImage} alt='A table full of delicious food!' />
+        <img src={fluffyImage} alt='A table full of delicious food!' />
       </div>
     </Fragment>
   );
